4bf1afc47c6556b63956b314a40f790f57797855
Sridevi <sridevi.krishnamurthi@sintef.no>
2025-01-21 13:48:16 +0100

    activation_protocol_test added

---
+++ tests/test_models/test_activation_protocol.py
@@ -0,0 +1,9 @@
+import pytest,os,sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(),'..',"..", 'examples')))
+print(sys.path)
+from test_protocols import activation_test_protocol
+
+
+def test_activation_protocol():
+    result= activation_test_protocol.activation_test_protocol()
+    assert isinstance(result, dict), "Expected a dictionary but got a different type"
